e0d328dd747d8eeaa6512b0cb5815c3896c88038
Jimmy Dickinson <jamesfdickinson@gmail.com>
2022-08-19 16:50:33 -0700

    first commit

---
+++ BookCipher.js
@@ -0,0 +1,23 @@
+class BookCipher {
+  constructor(key) {
+    this.key = key;
+  }
+  decrypt(text, keyText) {
+    let result = '';
+    //for each word in the text
+    const words = text.match(/\S+\s*/g);
+    const keys = keyText.split(' ');
+    //for each number in the key
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const word = words[key - 1];
+      //get first letter of the word
+      let firstLetter = word.charAt(0);
+      if (firstLetter == "(") firstLetter = word.charAt(1);
+      if (firstLetter == "[") firstLetter = word.charAt(1);
+      result += firstLetter;
+    }
+    return result;
+  }
+
+}
