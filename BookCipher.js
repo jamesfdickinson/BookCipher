638c74aea9a3a6766b2948bd68b0ad9d114c0a14
Jimmy Dickinson <jamesfdickinson@gmail.com>
2022-08-19 17:01:24 -0700

    decode as full words

--- BookCipher.js
+++ BookCipher.js
@@ -2,7 +2,7 @@ class BookCipher {
   constructor(key) {
     this.key = key;
   }
-  decrypt(text, keyText) {
+  decryptAsChar(text, keyText) {
     let result = '';
     //for each word in the text
     const words = text.match(/\S+\s*/g);
@@ -16,6 +16,20 @@ class BookCipher {
       if (firstLetter == "(") firstLetter = word.charAt(1);
       if (firstLetter == "[") firstLetter = word.charAt(1);
       result += firstLetter;
+    }
+    return result;
+  }
+  decryptAsWord(text, keyText) {
+    let result = '';
+    //for each word in the text
+    const words = text.match(/\S+\s*/g);
+    const keys = keyText.split(' ');
+    //for each number in the key
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const word = words[key - 1];
+
+      result += word;
     }
     return result;
   }
---
+++ README.md
@@ -0,0 +1,1 @@
+# BookCipher
