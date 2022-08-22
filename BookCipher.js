ea5ade1a7f7288c36dfbac1fdc11ab48520774d3
Jimmy Dickinson <jamesfdickinson@gmail.com>
2022-08-22 01:07:56 -0700

    add more decoders

--- BookCipher.js
+++ BookCipher.js
@@ -33,5 +33,32 @@ class BookCipher {
     }
     return result;
   }
+  decryptAsCharCount(text, keyText) {
+    let result = '';
+
+    const keys = keyText.split(' ');
+    //for each number in the key
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const textChar = text[key - 1];
+
+      result += textChar;
+    }
+    return result;
+  }
+  decryptAsCharCountIgnoreWhiteSpace(text, keyText) {
+    let result = '';
+    //regex remove all white space and return the text
+    const textNoWhiteSpace = text.replace(/[^a-z0-9]/gi, '');
+    const keys = keyText.split(' ');
+    //for each number in the key
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const textChar = textNoWhiteSpace[key - 1];
+
+      result += textChar;
+    }
+    return result;
+  }
 
 }
